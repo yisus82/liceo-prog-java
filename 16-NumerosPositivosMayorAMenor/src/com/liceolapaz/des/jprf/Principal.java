402d2d0826bff1e5fc259a30b019e91f04264752
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2022-11-21 10:57:45 +0100

    16-NumerosPositivosMayorAMenor v2

--- 16-NumerosPositivosMayorAMenor/src/com/liceolapaz/des/jprf/Principal.java
+++ 16-NumerosPositivosMayorAMenor/src/com/liceolapaz/des/jprf/Principal.java
@@ -1,71 +1,46 @@
 package com.liceolapaz.des.jprf;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Principal {
-	public static void main(String[] args) {
-		// Pedir los números
-		pedirNumeros();
-		// Variable para almacenar números
-		int[] numeros = new int[10];
-		// Variable para contar números almacenados
-		int numerosAlmacenados = 0;
-		while(true) {
-			// Leer el número y almacenarlo
-			int numero = leerNumero();
-			// Si es positivo
-			if (numero > 0) {
-				// Almacenarlo
-				numeros[numerosAlmacenados] = numero;
-				numerosAlmacenados++;
-			}
-			// Si no es positivo
-			else {
-				// Mostrar un mensaje de error
-				System.out.println("Por favor, introduzca solo números enteros positivos");
-				continue;
-			}
-			if (numerosAlmacenados == 10) {
-				break;
-			}
-		}
-		// Ordenar array
-		// Arrays.sort(numeros); (De menor a mayor)
-		ordenarNumeros(numeros);
-		// Escribir números
-		escribirNumeros(numeros);
-		// Escribir mensaje de despedida
-		System.out.println("¡Hasta la próxima!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Introduzca 10 números enteros positivos:");
+        int[] numeros = new int[10];
+        for (int i = 0; i < numeros.length; i++) {
+            int numero = leerNumero();
+            while (numero <= 0) {
+                System.out.println("Los números tienen que ser enteros positivos");
+                numero = leerNumero();
+            }
+            numeros[i] = numero;
+        }
+        int[] numerosOrdenados = ordenarNumeros(numeros);
+        System.out.print("Los números introducidos fueron: ");
+        for (int i = 0; i < numerosOrdenados.length; i++) {
+            System.out.print(numerosOrdenados[i] + " ");
+        }
+        System.out.println("\n¡Hasta la próxima!");
+    }
 
-	private static void ordenarNumeros(int[] numeros) {
-		int aux;
-		for(int i = 0; i < numeros.length - 1; i++) {
-			for (int j = i + 1; j < numeros.length; j++) {
-				if (numeros[j] > numeros[i]) {
-					aux = numeros[i];
-					numeros[i] = numeros[j];
-					numeros[j] = aux;
-				}
-			}
-		}
-	}
+    private static int[] ordenarNumeros(int[] numeros) {
+        int[] numerosOrdenados = new int[numeros.length];
+        for (int i = 0; i < numerosOrdenados.length; i++) {
+            int max = -1;
+            int maxPos = -1;
+            for (int j = 0; j < numeros.length; j++) {
+                if (numeros[j] > max) {
+                    max = numeros[j];
+                    maxPos = j;
+                }
+            }
+            numerosOrdenados[i] = max;
+            numeros[maxPos] = -1;
+        }
+        return numerosOrdenados;
+    }
 
-	private static void escribirNumeros(int[] numeros) {
-		System.out.print("Los números introducidos fueron:");
-		for (int i = 0; i < numeros.length; i++) {
-			System.out.print(" " + numeros[i]);
-		}
-		System.out.println();
-	}
-
-	private static int leerNumero() {
-		Scanner escaner = new Scanner(System.in);
-		return escaner.nextInt();
-	}
-
-	private static void pedirNumeros() {
-		System.out.println("Escriba 10 números enteros positivos enteros:");
-	}
+    private static int leerNumero() {
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextInt();
+    }
 }
